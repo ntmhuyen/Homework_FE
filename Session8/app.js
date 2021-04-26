fa7985a2cc48e436e72306543bef4e2d78b65381
minhhuyena2cma@gmail.com <minhhuyena2cma@gmail.com>
2021-04-27 00:25:10 +0700

    Bai tap s8 DOM

--- Session8/app.js
+++ Session8/app.js
@@ -1,5 +1,4 @@
 // JS HTML DOM 
-
 var btn1 = document.getElementById("btn1");
 console.log(btn1);
 var div1 = document.getElementById("div1");
@@ -21,15 +20,14 @@ var div3_1 = document.getElementById("div31");
 var div3_2 = document.getElementById("div32");
 
 btn3.onclick = function() {
-    var TG1 = div3_1.innerHTML;
-    var TG2 = div3_2.innerHTML;
-    div3_1.innerHTML = TG2;
-    div3_2.innerHTML = TG1;
+    var TG = div3_1.innerHTML;
+    div3_1.innerHTML = div3_2.innerHTML;
+    div3_2.innerHTML = TG;
 }
 
 var btn4 = document.getElementById("btn4");
 var div4 = document.getElementById("div4");
-var fSize = 14;
+var fSize = 14; 
 btn4.onclick = function() {
     fSize = fSize + 1;
     div4.style.fontSize = fSize +"px";
